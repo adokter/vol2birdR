7ae82d003e6b9b120f83629695c0b7bf85dbb4d3
Adriaan Dokter <adriaandokter@gmail.com>
2022-07-21 14:32:47 -0400

    add vol2bird_version()

---
+++ R/vol2bird_version.R
@@ -0,0 +1,15 @@
+#' Return vol2bird version
+#'
+#' Return version of the vol2bird algorithm
+#'
+#' @return an object of class \link{numeric_version}
+#'
+#' @export
+#' @examples
+#' # check installed vol2bird version:
+#' vol2bird_version()
+vol2bird_version <- function(){
+  constants_header_file <- file.path(find.package("vol2birdR"),"libvol2bird","constants.h")
+  assert_that(file.exists(constants_header_file))
+  numeric_version(strsplit(grep("VERSION ",readLines(file.path(find.package("vol2birdR"),"libvol2bird","constants.h")),value=TRUE),"\"")[[1]][2])
+}
