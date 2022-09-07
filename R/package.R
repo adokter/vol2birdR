0e0e7e86ac44561baebb73dea1ca0c7f9bf6d773
Adriaan Dokter <adriaandokter@gmail.com>
2022-09-07 10:21:51 -0400

    add hook to enable parallel libtorch

--- R/package.R
+++ R/package.R
@@ -7,6 +7,13 @@ globalVariables(c("..", "self", "private", "N"))
 }
 
 .onLoad <- function(libname, pkgname) {
+
+  # required environment variable on Mac for parallel use of libtorch:
+  osname<-tolower(Sys.info()[["sysname"]])
+  if (osname == "darwin" && Sys.getenv("OMP_NUM_THREADS") == "") {
+    Sys.setenv(OMP_NUM_THREADS = parallel::detectCores())
+  }
+
   cpp_vol2bird_namespace__store_main_thread_id()
   cpp_vol2bird_initialize()
   cpp_vol2bird_set_wsr88d_site_location(file.path(find.package(pkgname), "librsl", "wsr88d_locations.dat"))
