6371b78acb152d76e46f10d74ac05e01809d5a39
iskandari <sasha.tedeschi@gmail.com>
2023-05-05 16:38:02 -0400

    fixed error handling to use vol2brid_err_printf

--- src/libvol2bird/libvol2bird.c
+++ src/libvol2bird/libvol2bird.c
@@ -5108,7 +5108,7 @@ int vol2birdSetUp(PolarVolume_t* volume, vol2bird_t* alldata) {
     int radar_name_result = get_radar_name(PolarVolume_getSource(volume), alldata->misc.radarName, sizeof(alldata->misc.radarName));
     if (radar_name_result != 0) {
         // handle error
-        Rcpp::Rcout << "Warning: unable to extract radar name from source string" << std::endl;
+        vol2bird_err_printf("Warning: unable to extract radar name from source string\n")
     }
 
     // reading radar wavelength from polar volume attribute
