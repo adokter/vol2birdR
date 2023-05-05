b541e371f1deb73df205828a0418cdb9e09891de
iskandari <sasha.tedeschi@gmail.com>
2023-05-05 16:39:45 -0400

    nanify vpts() should be the same as nanify_str() in header

--- src/includes/libvol2bird/libvol2bird.h
+++ src/includes/libvol2bird/libvol2bird.h
@@ -488,7 +488,7 @@ double nanify(double value);
 
 void nanify_str(char* buff, const char* fmt, double v);
 
-char* nanify_vpts(float value, const char* fmt, double v);
+void nanify_vpts(char* buff, const char* fmt, double v);
 
 void create_profile_printout_str(char* printbuffer, int buflen, const char* date, const char* time,
     float HGHT, float u, float v, float w, float ff, float dd, float sd_vvp, char gap, float dbz,
